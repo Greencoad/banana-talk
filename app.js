305ada959d2207f322f69f279011c893d9d3e04e
Greencoad <64676251+Greencoad@users.noreply.github.com>
2021-01-04 09:52:08 +0530

    removinf _id Signs

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var btnTranslate = document.querySelector("#btn-translate");
-var txtInput = document.querySelector("#txt-input");
+var btnTranslate = document.querySelector("btn-translate");
+var txtInput = document.querySelector("txt-input");
 var outputDiv = document.querySelector("output")
 
  var serverURL = "https://api.funtranslations.com/translate/minion.json"
